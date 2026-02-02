4da4f3c89ce1ee98868dedd9ed63795dfd77a34e
Leo <le4ndro.fioretti@gmail.com>
2026-02-02 14:36:17 +0100

    initial ver

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+import { Button } from "@/components/ui/button";
+
+const greetMsg = ref("");
+const name = ref("");
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsg.value = await invoke("greet", { name: name.value });
+}
+</script>
+
+<template>
+  <main class="container">
+    <Button>click me</Button>
+  </main>
+</template>
